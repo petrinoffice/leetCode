e639eea9d634eb6ca351ba2e5ca767fddebfae4e
Petr <petrinoffice@gmail.com>
2024-07-30 08:57:56 +0200

    Update

---
+++ src/leet/N104_Maximum_Depth_Binary_Tree.java
@@ -0,0 +1,51 @@
+package leet;
+
+import java.util.Objects;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class N104_Maximum_Depth_Binary_Tree {
+    public static void main(String[] args) {
+
+        TreeNode treeNode15 = new TreeNode(15);
+        TreeNode treeNode7 = new TreeNode(7);
+
+        TreeNode treeNode9 = new TreeNode(9);
+
+        TreeNode treeNode20 = new TreeNode(20, treeNode15, treeNode7);
+
+        TreeNode treeNode3 = new TreeNode(3, treeNode9, treeNode20);
+        System.out.println(new N104_Maximum_Depth_Binary_Tree().maxDepth(treeNode3));
+    }
+
+
+    public int maxDepth(TreeNode root) {
+        if (Objects.isNull(root)) {
+            return 0;
+        }
+
+        int leftDeep = maxDepth(root.left);
+        int rightDeep = maxDepth(root.right);
+
+        return Math.max(leftDeep, rightDeep)+1;
+    }
+
+
+}
--- src/leet/N228_Summary_Ranges.java
+++ src/leet/N228_Summary_Ranges.java
@@ -1,6 +1,5 @@
 package leet;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
